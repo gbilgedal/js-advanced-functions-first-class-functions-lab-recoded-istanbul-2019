d9ac7b1030f0b73b3a0323e76bcd4471625d4e51
G Bilge Dal <lagala999@gmail.com>
2020-04-28 10:43:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const returnFirstTwoDrivers = function(array){
   return array.slice(0,2);
 }
-const returnFirstTwoDrivers = function(array){
-  return array.slice(array.length-3,array.length-1);
+const returnLastTwoDrivers = function(array){
+  return array.slice(array.length-3,array.length);
 }
